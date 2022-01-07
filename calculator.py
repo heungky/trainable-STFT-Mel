ed209b3fe1c48ad6d3dd8e8102cc89fb49dcb494
heungky <heungky@gmail.com>
2022-01-07 13:33:40 +0800

    add subtract and division

--- calculator.py
+++ calculator.py
@@ -6,19 +6,18 @@ import argparse
 calculator = argparse.ArgumentParser(description='Calculator')
 
 
-
 # add the arguments (must)
 # metavar: provides a diff name for optional argument in help messages 
 # indicate how many variable gonna accept
 
 calculator.add_argument('Number1',metavar='1st number', type=float, 
-                        help='the number that you want to add')
+                        help='the number that you want to process')
 
 calculator.add_argument('Number2',metavar='2nd number', type=float, 
-                        help='the number that you want to add')
+                        help='the number that you want to process')
 
 
-calculator.add_argument('--mode', type=str, help='type of function you want to use in calculator')
+calculator.add_argument('--mode', type=str, help='Type of function you want to use in calculator.Can choose sum/ multiply/ subtract/ division')
 
 
 def summation(a,b):
@@ -27,15 +26,28 @@ def summation(a,b):
 def multiply(a,b):
     return a*b
 
+def subtraction(a,b):
+    return a-b
+
+def division(a,b):
+    return a/b
+
+
+
 # execute the parse_args() method
 # take argument become variable 
 number1 = calculator.parse_args().Number1
 number2 = calculator.parse_args().Number2
-mode = calculator.parse_args().mode
 
-if mode=='summation':
+function = calculator.parse_args().mode
+
+if function=='sum':
     print(summation(number1, number2))
-elif mode=='multiply':
+elif function=='multiply':
     print(multiply(number1, number2))
+elif function=='subtract':
+    print(subtraction(number1, number2))
+elif function=='division':
+    print(division(number1, number2))
 else:
-    pass
+    print(f'Please try again, type sum/ multiply/ substract/ division')
