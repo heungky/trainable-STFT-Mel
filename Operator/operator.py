9bb29fca6d0be9eda582e3f8f10386d2fe7c6625
heungky <heungky@gmail.com>
2022-01-07 13:53:17 +0800

    add operator file

---
+++ Operator/operator.py
@@ -0,0 +1,17 @@
+class Summation():
+    def __call__(self, a,b):
+        return a+b
+
+class Multiply():
+    def __call__(self, a,b):
+        return a*b 
+        
+class Subtraction():
+    def __call__(self, a,b):
+        return a-b 
+    
+class Division():
+    def __call__(self, a,b):
+        return a/b     
+    
+     
--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 # calculator.py
 import argparse
-
+import Operator.operator as Op
 
 # create the parser
 calculator = argparse.ArgumentParser(description='Calculator')
@@ -20,17 +20,20 @@ calculator.add_argument('Number2',metavar='2nd number', type=float,
 calculator.add_argument('--mode', type=str, help='Type of function you want to use in calculator.Can choose sum/ multiply/ subtract/ division')
 
 
-def summation(a,b):
-    return a+b
+# class Operator():
+#     def summation(self, a,b):
+#         return a+b
 
-def multiply(a,b):
-    return a*b
+#     def multiply(self, a,b):
+#         return a*b
 
-def subtraction(a,b):
-    return a-b
+#     def subtraction(self, a,b):
+#         return a-b
 
-def division(a,b):
-    return a/b
+#     def division(self, a,b):
+#         return a/b    
+
+
 
 
 
@@ -41,13 +44,17 @@ number2 = calculator.parse_args().Number2
 
 function = calculator.parse_args().mode
 
-if function=='sum':
-    print(summation(number1, number2))
-elif function=='multiply':
-    print(multiply(number1, number2))
-elif function=='subtract':
-    print(subtraction(number1, number2))
-elif function=='division':
-    print(division(number1, number2))
-else:
-    print(f'Please try again, type sum/ multiply/ substract/ division')
+operator = getattr(Op, function)()
+
+print(operator(number1, number2))
+
+# if function=='sum':
+#     print(summation(number1, number2))
+# elif function=='multiply':
+#     print(multiply(number1, number2))
+# elif function=='subtract':
+#     print(subtraction(number1, number2))
+# elif function=='division':
+#     print(division(number1, number2))
+# else:
+#     print(f'Please try again, type sum/ multiply/ substract/ division')
