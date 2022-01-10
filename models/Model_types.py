135e8aae1d2ef3de7ac3f3c8e9c99827b3b003b4
heungky <heungky@gmail.com>
2022-01-10 14:09:17 +0800

    add pytorch_lighting and tensorboard

--- models/Model_types.py
+++ models/Model_types.py
@@ -1,20 +1,44 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from pytorch_lightning.core.lightning import LightningModule
+import torch.optim as optim
+from nnAudio.features.mel import MelSpectrogram
+import sys
 
-class ModelA(nn.Module):
+class ModelA(LightningModule):
     def __init__(self, no_output_chan):
         super().__init__()
-        print(f"I am model A")        
+        print(f"I am model A")   
+        self.mel_layer = MelSpectrogram(sr=16000, 
+                                   n_fft=2048,
+                                   win_length=None,
+                                   n_mels=100, 
+                                   hop_length=512,
+                                   window='hann',
+                                   center=True,
+                                   pad_mode='reflect',
+                                   power=2.0,
+                                   htk=False,
+                                   fmin=0.0,
+                                   fmax=None,
+                                   norm=1,
+                                   trainable_mel=False,
+                                   trainable_STFT=False,
+                                   verbose=True,)        
+        
         self.conv1 = nn.Conv2d(1,no_output_chan,5)    
         self.conv2 = nn.Conv2d(no_output_chan,16,5)
+        
+      
         self.fc1 = nn.Linear(16*22*5,120) 
         #have to follow input, x.shape before flattern: 
         self.fc2 = nn.Linear(120,84)
         self.fc3 = nn.Linear(84,35)
         
+        self.criterion = nn.CrossEntropyLoss()
+        
     def forward(self,x):
-        #print(f"{x.shape=}")
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
         x = F.max_pool2d(F.relu(self.conv2(x)),2)
       
@@ -26,13 +50,49 @@ class ModelA(nn.Module):
         return x
     
     
+    def training_step(self, batch, batch_idx):
+        mel_output_batch = self.mel_layer(batch['waveforms']) 
+        mel_output_batch4 = mel_output_batch.unsqueeze(1) 
+        
+        outputs = self(mel_output_batch4)
+        loss = self.criterion(outputs, batch['labels'].squeeze(1).long())
+#         torch.save(outputs, 'output.pt')
+#         torch.save(batch['labels'], 'label.pt')        
+#for debug   
+#         sys.exit()
+
+
+        acc = sum(outputs.argmax(-1) == batch['labels'].squeeze(1))/outputs.shape[0]
+        
+        self.log('Train/acc', acc, on_step=False, on_epoch=True)
+        self.log('Train/Loss', loss, on_step=False, on_epoch=True)
+        return loss
     
-class ModelB(nn.Module):
+    
+    
+    def validation_step(self, batch, batch_idx):
+        mel_output_batch = self.mel_layer(batch['waveforms']) 
+        mel_output_batch4 = mel_output_batch.unsqueeze(1) 
+        
+        outputs = self(mel_output_batch4)
+        loss = self.criterion(outputs, batch['labels'].squeeze(1).long())
+        
+        acc = sum(outputs.argmax(-1) == batch['labels'].squeeze(1))/outputs.shape[0]
+        self.log('Validation/acc', acc, on_step=False, on_epoch=True)
+        self.log('Validation/Loss', loss, on_step=False, on_epoch=True)    
+        
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=0.001)
+   
+    
+    
+class ModelB(LightningModule):
     def __init__(self, no_output_chan):
         super().__init__()
         print(f"I am model B")
         self.conv1 = nn.Conv2d(1,no_output_chan,5)    
         self.conv2 = nn.Conv2d(no_output_chan,16,5)
+        
         self.fc1 = nn.Linear(16*22*5,120) 
         #have to follow input, x.shape before flattern: 
         self.fc2 = nn.Linear(120,100)
--- speechcommand_arg.py
+++ speechcommand_arg.py
@@ -8,50 +8,41 @@ import torch.nn.functional as F
 import pandas
 import tqdm
 from torch.nn.utils.rnn import pad_sequence
-from nnAudio.features.mel import MelSpectrogram
 import matplotlib.pyplot as plt
 from IPython.display import Audio
 from dataloading_util import data_processing
 import models as Model 
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
+from datetime import datetime
 
 
 speechcommand_arg = argparse.ArgumentParser(description='User choose GPU or CPU')
 
-speechcommand_arg.add_argument('--device', type=str, help='choose either GPU or CPU')
-speechcommand_arg.add_argument('--output_chan', type=int, help='Number of output channel')
-speechcommand_arg.add_argument('--model_type', type=str, help='choose model type')
+speechcommand_arg.add_argument('--device','-d', type=int, help='choose either GPU [1] or CPU[0]')
+speechcommand_arg.add_argument('--output_chan','-c', type=int, help='Number of output channel')
+speechcommand_arg.add_argument('--model_type','-t', type=str, help='choose model type')
+speechcommand_arg.add_argument('--epoch', '-e', type=int, help='number of epoch')
 
 
 device = speechcommand_arg.parse_args().device
 no_output_chan = speechcommand_arg.parse_args().output_chan
 model_type = speechcommand_arg.parse_args().model_type
+max_epochs = speechcommand_arg.parse_args().epoch
 
 
-mel_layer = MelSpectrogram(sr=16000, 
-                           n_fft=2048,
-                           win_length=None,
-                           n_mels=100, 
-                           hop_length=512,
-                           window='hann',
-                           center=True,
-                           pad_mode='reflect',
-                           power=2.0,
-                           htk=False,
-                           fmin=0.0,
-                           fmax=None,
-                           norm=1,
-                           trainable_mel=False,
-                           trainable_STFT=False,
-                           verbose=True,)
-
-mel_layer.to(device)
 
 batch_size = 8
+
 trainset = torchaudio.datasets.SPEECHCOMMANDS('./',url='speech_commands_v0.02',folder_in_archive='SpeechCommands',download = False, subset = 'training')
 testset = torchaudio.datasets.SPEECHCOMMANDS('./',url='speech_commands_v0.02',folder_in_archive='SpeechCommands',download = False, subset = 'testing')
+validset = torchaudio.datasets.SPEECHCOMMANDS('./',url='speech_commands_v0.02',folder_in_archive='SpeechCommands',download = False, subset = 'validation')
+
+
 
 trainloader = torch.utils.data.DataLoader(trainset,
                               batch_size=batch_size,
+                              shuffle=True,
                               collate_fn=lambda x: data_processing(x))
 
 #speech_command_transform = Speech_Command_label_Transform(trainset)
@@ -59,34 +50,44 @@ testloader = torch.utils.data.DataLoader(testset,
                               batch_size=batch_size,
                               collate_fn=lambda x: data_processing(x))
 
+validloader = torch.utils.data.DataLoader(validset,
+                              batch_size=batch_size,
+                              collate_fn=lambda x: data_processing(x))
+#for dataloader, trainset need shuffle
 
-
+#***********
 net = getattr(Model, model_type)(no_output_chan)
-net = net.to(device)
-import torch.optim as optim
+# net = net.to(device)
+print(type(net))
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-for epoch in range(2):
-    Loss = 0
+
+#added into Model_types.py, using pytorch_lightning
+#criterion = nn.CrossEntropyLoss()
+#optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+#for epoch in range(2):
+    #Loss = 0
     
-    for batch in tqdm.tqdm(trainloader):
-        mel_output_batch = mel_layer(batch['waveforms'].to(device)) 
-        mel_output_batch4 = mel_output_batch.unsqueeze(1) 
+    #for batch in tqdm.tqdm(trainloader):
+        #mel_output_batch = mel_layer(batch['waveforms'].to(device)) 
+        #mel_output_batch4 = mel_output_batch.unsqueeze(1) 
         
-        optimizer.zero_grad()
-        outputs = net(mel_output_batch4)
-        loss = criterion(outputs, batch['labels'].to(device).squeeze(1).long()) 
-
-        loss.backward()
-        optimizer.step()
         
+        #optimizer.zero_grad()
+        #outputs = net(mel_output_batch4)
+        #loss = criterion(outputs, batch['labels'].to(device).squeeze(1).long()) 
+
+        #loss.backward()
+        #optimizer.step()
+        
+now = datetime.now()        
+logger = TensorBoardLogger(save_dir=".", version=1, name=f'output/{now.strftime("%d-%m-%Y-%H-%M-%S")}')
 
 
-
-
-
+trainer = Trainer(gpus=device, max_epochs=max_epochs,logger=logger, check_val_every_n_epoch=1)
+trainer.fit(net, trainloader, validloader)
+#added validloader, in order to reach validation_step
 
 
 
