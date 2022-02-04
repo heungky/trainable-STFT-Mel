8ce8f029d04e0efa4f483f55eee9c39befb1747a
KinWaiCheuk <u3500684@connect.hku.hk>
2022-02-04 09:19:23 +0800

    added confusion matrix

--- models/lightning_module.py
+++ models/lightning_module.py
@@ -14,7 +14,11 @@ from cosine_annealing_warmup import CosineAnnealingWarmupRestarts
 from sklearn.metrics import precision_recall_fscore_support
 from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 from sklearn.metrics import precision_recall_fscore_support
-from dataset.speechcommands import idx2name
+from dataset.speechcommands import idx2name, name2idx
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import re
+import itertools
 
 
 class SpeechCommand(LightningModule):
@@ -37,8 +41,15 @@ class SpeechCommand(LightningModule):
         self.log('Train/acc', acc, on_step=False, on_epoch=True)
         #if self.current_epoch==0:
         if batch_idx == 0:
-            self.log_images(spec, 'Train/Spec')        
+            self.log_images(spec, 'Train/Spec')
+            cm = plot_confusion_matrix(batch['labels'].cpu(),
+                                       outputs.argmax(-1).cpu(),
+                                       name2idx.keys(),
+                                       title='Train: Confusion matrix',
+                                       normalize=False)
+            self.logger.experiment.add_figure('Train/confusion_maxtrix', cm, global_step=self.current_epoch)            
         self.log('Train/Loss', loss, on_step=False, on_epoch=True)
+        
         return loss
 #log(graph title, take acc as data, on_step: plot every step, on_epch: plot every epoch)
                     
@@ -276,3 +287,59 @@ def barplot(result_dict, title, figsize=(4,12), minor_interval=0.2, log=False):
 #         fig.set_tight_layout(True)
     return fig
           
+    
+def plot_confusion_matrix(correct_labels,
+                          predict_labels,
+                          labels,
+                          title='Confusion matrix',
+                          normalize=False):
+    ''' 
+    Parameters:
+        correct_labels                  : These are your true classification categories.
+        predict_labels                  : These are you predicted classification categories
+        labels                          : This is a lit of labels which will be used to display the axix labels
+        title='Confusion matrix'        : Title for your matrix
+        tensor_name = 'MyFigure/image'  : Name for the output summay tensor
+
+    Returns:
+        summary: TensorFlow summary 
+
+    Other itema to note:
+        - Depending on the number of category and the data , you may have to modify the figzie, font sizes etc. 
+        - Currently, some of the ticks dont line up due to rotations.
+    '''
+    cm = confusion_matrix(correct_labels, predict_labels)
+    if normalize:
+        cm = cm.astype('float')*10 / cm.sum(axis=1)[:, np.newaxis]
+        cm = np.nan_to_num(cm, copy=True)
+        cm = cm.astype('int')
+
+    np.set_printoptions(precision=2)
+
+    fig, ax = plt.subplots(1, 1, figsize=(7, 7), dpi=160, facecolor='w', edgecolor='k')
+    im = ax.imshow(cm, cmap='Oranges')
+
+    classes = [re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r'\1 ', x) for x in labels]
+#     classes = ['\n'.join(l) for l in classes]
+
+    tick_marks = np.arange(len(classes))
+
+    ax.set_xlabel('Predicted', fontsize=7)
+    ax.set_xticks(tick_marks)
+    c = ax.set_xticklabels(classes, fontsize=6, rotation=0,  ha='center')
+    ax.xaxis.set_label_position('bottom')
+    ax.xaxis.tick_bottom()
+
+    ax.set_ylabel('True Label', fontsize=7)
+    ax.set_yticks(tick_marks)
+    ax.set_yticklabels(classes, fontsize=6, va ='center')
+    ax.yaxis.set_label_position('left')
+    ax.yaxis.tick_left()
+
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        ax.text(j, i, format(cm[i, j], 'd') if cm[i,j]!=0 else '.', horizontalalignment="center", fontsize=6, verticalalignment='center', color= "black")
+    fig.set_tight_layout(True)
+#     summary = tfplot.figure.to_summary(fig, tag=tensor_name)
+#     return summary
+
+    return fig
