488698c66719e69fb75156db9695c90adeb00d36
heungky <heungky@gmail.com>
2022-02-14 17:36:02 +0800

    add BCResNet_nnAudio_ASR model with nnAudio for ASR task

--- models/nnAudio_model.py
+++ models/nnAudio_model.py
@@ -124,6 +124,115 @@ class BCResNet_nnAudio(SpeechCommand):
 #old spec :4D [B,1,F,T] , the return spec is for plot log_images, so need 3D
         return out, spec
 
+class BCResNet_nnAudio_ASR(ASR):
+    def __init__(self,cfg_model,text_transform,lr): 
+        #in main script, will pass no_output_chan, cfg_spec to model
+        super().__init__(text_transform,lr)
+        self.fastaudio_filter = None
+        self.optimizer_cfg = cfg_model.optimizer
+        self.cfg_model = cfg_model
+        self.conv1 = nn.Conv2d(1, 16, 5, stride=(2, 1), padding=(2, 2))
+        self.block1_1 = TransitionBlock(16, 8)
+        self.block1_2 = BroadcastedBlock(8)
+
+        self.block2_1 = TransitionBlock(8, 12, stride=(2, 1), dilation=(1, 2), temp_pad=(0, 2))
+        self.block2_2 = BroadcastedBlock(12, dilation=(1, 2), temp_pad=(0, 2))
+
+        self.block3_1 = TransitionBlock(12, 16, stride=(2, 1), dilation=(1, 4), temp_pad=(0, 4))
+        self.block3_2 = BroadcastedBlock(16, dilation=(1, 4), temp_pad=(0, 4))
+        self.block3_3 = BroadcastedBlock(16, dilation=(1, 4), temp_pad=(0, 4))
+        self.block3_4 = BroadcastedBlock(16, dilation=(1, 4), temp_pad=(0, 4))
+
+        self.block4_1 = TransitionBlock(16, 20, dilation=(1, 8), temp_pad=(0, 8))
+        self.block4_2 = BroadcastedBlock(20, dilation=(1, 8), temp_pad=(0, 8))
+        self.block4_3 = BroadcastedBlock(20, dilation=(1, 8), temp_pad=(0, 8))
+        self.block4_4 = BroadcastedBlock(20, dilation=(1, 8), temp_pad=(0, 8))
+
+        self.conv2 = nn.Conv2d(20, 20, 5, groups=20, padding=(0, 2))
+        self.conv3 = nn.Conv2d(20, 32, 1, bias=False)
+        self.conv4 = nn.Conv2d(32,  self.cfg_model.args.output_dim, 1, bias=False)
+                
+        self.mel_layer = MelSpectrogram(**cfg_model.spec_args)
+        self.criterion = nn.CrossEntropyLoss()        
+        #self.mel_layer use in validation step for [mel_filter_banks = self.mel_layer.mel_basis]
+        #self.criterion use in traning & validation step
+        
+#         self.norm = InputNormalization()
+        #for normalization
+        
+    def forward(self, x):        
+        # x [Batch_size,16000]
+#         self.mel_layer.mel_basis = torch.clamp(self.mel_layer.mel_basis, 0, 1)
+        spec = self.mel_layer(x) # [B,F,T]
+        # (B, 40, T)
+        #print(f'{spec.max()=}')
+        #print(f'{spec.min()=}')
+        
+        #spec = torch.relu(spec)
+        
+        spec = torch.log(spec+1e-10) 
+        
+#         batch_size = torch.ones([spec.shape[0]]).to(spec.device)                
+#         self.norm.to(spec.device)
+#         spec = self.norm(spec, batch_size)
+#for normalization
+        
+        spec = spec.unsqueeze(1)
+#x is training_step_batch['waveforms' [B,16000]
+#after self.mel_layer(x) --> 3D [B,F,T]
+#after spec.unsqueeze(1) --> 4D bcoz conv1 need 4D [B,1,F,T]
+
+
+#         print('INPUT SHAPE:', x.shape)
+#         print('INPUT spec SHAPE:', spec.shape)
+        out = self.conv1(spec)
+
+#         print('BLOCK1 INPUT SHAPE:', out.shape)
+        out = self.block1_1(out)
+        out = self.block1_2(out)
+
+#         print('BLOCK2 INPUT SHAPE:', out.shape)
+
+        out = self.block2_1(out)
+        
+#         print('middle BLOCK2 INPUT SHAPE:', out.shape)
+        
+        out = self.block2_2(out)
+
+#         print('BLOCK3 INPUT SHAPE:', out.shape)
+        out = self.block3_1(out)
+        out = self.block3_2(out)
+        out = self.block3_3(out)
+        out = self.block3_4(out)
+
+#         print('BLOCK4 INPUT SHAPE:', out.shape)
+        out = self.block4_1(out)
+        out = self.block4_2(out)
+        out = self.block4_3(out)
+        out = self.block4_4(out)
+
+#         print('Conv2 INPUT SHAPE:', out.shape)
+        out = self.conv2(out)
+
+#         print('Conv3 INPUT SHAPE:', out.shape)
+        out = self.conv3(out)
+#         out = out.mean(-1, keepdim=True) 
+# after taking mean, time dimension is gone, diff setting from KWS
+
+#         print('Conv4 INPUT SHAPE:', out.shape)
+        out = self.conv4(out)   #4D
+        out = out.squeeze(2) #.squeeze(2)  #3D (B,F,T)
+        out = out.transpose(1,2) ##D (B,T,F)
+        
+        spec = spec.squeeze(1)
+
+#         print('OUTPUT SHAPE:', out.shape)
+#        OUTPUT SHAPE: torch.Size([8, 35, 1, 1])  out
+
+#crossentropy expect[B, C], so need to squeeze to be 2 dimension
+#ref:https://pytorch.org/docs/1.9.1/generated/torch.nn.CrossEntropyLoss.html
+#old spec :4D [B,1,F,T] , the return spec is for plot log_images, so need 3D
+        return out, spec
 
 
 class BCResNet_exp_nnAudio(SpeechCommand):        
