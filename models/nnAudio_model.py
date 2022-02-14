f086d41ac8faa90a8505bbcaf3d739007f867a70
heungky <heungky@gmail.com>
2022-02-14 11:13:01 +0800

    can control input & output_dim in BC_ResNet_exp config file

--- models/nnAudio_model.py
+++ models/nnAudio_model.py
@@ -126,11 +126,12 @@ class BCResNet(SpeechCommand):
 
 
 class BCResNet_exp(SpeechCommand):        
-    def __init__(self, no_output_chan, cfg_model): 
+    def __init__(self, cfg_model): 
         #in main script, will pass no_output_chan, cfg_spec to model
         super().__init__()
         self.fastaudio_filter = None
         self.optimizer_cfg = cfg_model.optimizer
+        self.cfg_model = cfg_model
         self.conv1 = nn.Conv2d(1, 16, 5, stride=(2, 1), padding=(2, 2))
         self.block1_1 = TransitionBlock(16, 8)
         self.block1_2 = BroadcastedBlock(8)
@@ -150,7 +151,7 @@ class BCResNet_exp(SpeechCommand):
 
         self.conv2 = nn.Conv2d(20, 20, 5, groups=20, padding=(0, 2))
         self.conv3 = nn.Conv2d(20, 32, 1, bias=False)
-        self.conv4 = nn.Conv2d(32, 35, 1, bias=False)
+        self.conv4 = nn.Conv2d(32, self.cfg_model.args.output_dim, 1, bias=False)
         
         self.mel_layer = MelSpectrogram(**cfg_model.spec_args)
         self.criterion = nn.CrossEntropyLoss()
